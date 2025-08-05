6bf0076a0c88bf3ee7ae9349444912409c799a28
sakib-atreus <sakibatreus@gmail.com>
2025-08-05 10:49:32 +0600

    refactor: set the client contact info with map

--- src/reuseable/Footer.jsx
+++ src/reuseable/Footer.jsx
@@ -9,7 +9,9 @@ const Footer = () => {
         <aside>
           <img src={footerLogo} alt="" />
           <p className="text-[#FFFFFF] leading-[150%] font-normal text-[14px] mt-2">
-          AI-powered interview preparation platform. <br /> Practice with real-time simulations, test your skills, and <br /> develop your skills and build confidence <br /> through live video interviews.
+            AI-powered interview preparation platform. <br /> Practice with
+            real-time simulations, test your skills, and <br /> develop your
+            skills and build confidence <br /> through live video interviews.
           </p>
           <div className="flex gap-2 justify-items-center mt-8">
             <input
@@ -29,28 +31,95 @@ const Footer = () => {
           <h6 className="text-[16px] text-[#37B874] font-medium leading-[130%] mb-5">
             Quick Links
           </h6>
-          <a href="/" className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Home</a>
-          <a href="/features" className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Features</a>
-          <a href="pricing" className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Pricing</a>
+          <a
+            href="/"
+            className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+          >
+            Home
+          </a>
+          <a
+            href="/features"
+            className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+          >
+            Features
+          </a>
+          <a
+            href="pricing"
+            className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+          >
+            Pricing
+          </a>
           {/* <a href="About-Us" className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">About Us</a> */}
-          <a href="/features#feature" className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Blog</a>
-          <a href="Contact-Us" className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Contact Us</a>
+          <a
+            href="/features#feature"
+            className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+          >
+            Blog
+          </a>
+          <a
+            href="Contact-Us"
+            className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+          >
+            Contact Us
+          </a>
         </nav>
         <nav>
           <h6 className="text-[16px] text-[#37B874] font-medium leading-[130%] mb-5">
             Legal & Support
           </h6>
-          <a href="/privacy-policy" className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Privacy Policy</a>
-          <a href="/terms-and-conditions" className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Terms of Service</a>
-          <a href="/Contact-Us" className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Help Center</a>
+          <a
+            href="/privacy-policy"
+            className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="/terms-and-conditions"
+            className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+          >
+            Terms of Service
+          </a>
+          <a
+            href="/Contact-Us"
+            className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+          >
+            Help Center
+          </a>
         </nav>
         <nav>
           <h6 className="text-[16px] text-[#37B874] font-medium leading-[130%] mb-5">
             Contact Info
           </h6>
-          <a className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Phone: +123 456 7890</a>
-          <a className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Email: info@inprepai.com</a>
-          <a className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]">Address: 123 Main Street, <br/>Your City, Your Country</a>
+          <p className="text-[#E0E0E1] text-[14px] font-medium leading-[130%]">
+            Phone:{" "}
+            <a
+              href="tel:+19432709658"
+              className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+            >
+              +1 9432709658
+            </a>
+          </p>
+          <p className="text-[#E0E0E1] text-[14px] font-medium leading-[130%]">
+            Email:{" "}
+            <a
+              href="mailto:info@inprepai.com"
+              className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+            >
+              info@inprepai.com
+            </a>
+          </p>
+          <p className="text-[#E0E0E1] text-[14px] font-medium leading-[130%]">
+            Address:{" "}
+            <a
+              href="https://www.google.com/maps?q=13010+Morris+Road,+Suite+670,+Alpharetta,+GA,+30004"
+              target="_blank"
+              className="link link-hover text-[#E0E0E1] text-[14px] font-medium leading-[130%]"
+            >
+              Inprep, A Hyrworx Inc. product{" "}
+              <p className="pl-16">13010 Morris Road, Suite 670,</p>{" "}
+              <p className="pl-16">Alpharetta, GA, 30004</p>{" "}
+            </a>
+          </p>
         </nav>
       </footer>
     </div>
